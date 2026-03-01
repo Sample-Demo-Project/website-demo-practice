e4e48631e9687060381c3c01bc0eec7fa5ca46ee
CheungHW <jenniferchw0902@gmail.com>
2026-03-01 17:52:17 +0800

    finish practice217

--- practices/practice-217/js/main.js
+++ practices/practice-217/js/main.js
@@ -1,5 +1,14 @@
 // TODO 1：建立一個陣列，放三段文字訊息
-
+let messages = [
+    "第一段訊息：歡迎來到我們的網站！",
+    "第二段訊息：我們提供各種有趣的內容。",
+    "第三段訊息：請隨時聯繫我們！"
+];
+console.log(messages);
 
 // TODO 2：完成 showMessage function
+function showMessage(index) {
+    const output = document.getElementById("output");
+    output.textContent = messages[index];
+}
 // 接收 index，並顯示對應的陣列內容到 output
